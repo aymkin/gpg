0e56b551f49f7dbda19ca2c4124a282bf92469e9
Oleksandr Naumkin <aymkin@gmail.com>
2024-04-26 15:33:59 +0300

    keep secrets in one folder

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $PWD/secret.txt secret.txt.gpg
+--output $PWD/secrets/secret.txt secret.txt.gpg
